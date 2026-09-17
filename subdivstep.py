06a710b1a5dacb8d2d207bc05bc5d167e32f7afd
hifisher221 <hifisher@engineering.upenn.edu>
2026-09-17 19:05:28 -0400

    Update subdivstep.py

--- subdivstep.py
+++ subdivstep.py
@@ -1,1 +1,17 @@
+import numpy as np
+from subdecas import subdecas
 
+def subdivstep(lpoly):
+
+  n = lpoly.shape[2]
+
+newpoly = np.zeros((2,4,3 *n))
+
+for i in range(n):
+
+  ud, ld = subdecas(lpoly[:, :, i])
+
+  newpoly[:, :, 2 * i] = ud
+  newpoly[:, :, 2 * i + 1] = ld
+
+return newpoly
